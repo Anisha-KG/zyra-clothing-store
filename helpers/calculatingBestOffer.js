6d8a554732ece63f66a4ed7431683411a243de95
Anisha K G <anishakg21@gmail.com>
2026-03-13 03:19:34 +0530

    best offer

--- helpers/calculatingBestOffer.js
+++ helpers/calculatingBestOffer.js
@@ -5,70 +5,61 @@ const Brand = require('../models/brandsSchema');
 
 async function calculateBestOffer(product) {
 
+    const now = Date.now(); 
     let offers = [];
 
- 
+
     if (
         product.offer &&
-        product.startDate <= new Date() &&
-        product.offerValidUntil >= new Date()
+        new Date(product.startDate).getTime() <= now &&
+        new Date(product.offerValidUntil).getTime() >= now
     ) {
-        offers.push(product.offer);
+        offers.push(Number(product.offer));
     }
 
-   
+
     const category = await Category.findById(product.category);
-
-    console.log("CATEGORY:", category);
-
-if (category) {
-    console.log("Offer:", category.categoryOffer);
-    console.log("Start:", category.startDate);
-    console.log("End:", category.endDate);
-}
 
     if (
         category &&
         category.categoryOffer &&
-        category.startDate <= new Date() &&
-        category.endDate >= new Date()
+        new Date(category.startDate).getTime() <= now &&
+        new Date(category.endDate).getTime() >= now
     ) {
-         console.log("Category offer added");
-        offers.push(category.categoryOffer);
+        offers.push(Number(category.categoryOffer));
     }
 
-   
+
     const subcategory = await Subcategory.findById(product.subcategory);
 
     if (
         subcategory &&
         subcategory.offer &&
-        subcategory.startDate <= new Date() &&
-        subcategory.endDate >= new Date()
+        new Date(subcategory.startDate).getTime() <= now &&
+        new Date(subcategory.endDate).getTime() >= now
     ) {
-        offers.push(subcategory.offer);
+        offers.push(Number(subcategory.offer));
     }
 
-    
+
     const brand = await Brand.findById(product.brand);
 
     if (
         brand &&
         brand.brandOffer &&
-        brand.startDate <= new Date() &&
-        brand.endDate >= new Date()
+        new Date(brand.startDate).getTime() <= now &&
+        new Date(brand.endDate).getTime() >= now
     ) {
-        offers.push(brand.brandOffer);
+        offers.push(Number(brand.brandOffer));
     }
 
-    
-    if (offers.length === 0) return 0;
 
-    console.log("All Offers:", offers);
-    console.log("Best Offer:", Math.max(...offers));
-    
+    if (offers.length === 0) {
+        return 0;
+    }
+
+   
     return Math.max(...offers);
 }
 
-module.exports = 
-    calculateBestOffer
+module.exports = calculateBestOffer;
